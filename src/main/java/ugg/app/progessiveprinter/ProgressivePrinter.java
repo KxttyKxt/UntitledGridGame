7225511eaa7c1b81649b8991f3332cad8cdc1d6a
Kat R. <56617952+KxttyKxt@users.noreply.github.com>
2025-02-20 11:50:54 -0500

    Implement delay and pause-with-delay for ProgressivePrinter

--- src/main/java/ugg/app/progessiveprinter/ProgressivePrinter.java
+++ src/main/java/ugg/app/progessiveprinter/ProgressivePrinter.java
@@ -9,6 +9,7 @@ public class ProgressivePrinter {
     private int charLimitPerLine;
 
     private final Scanner consolePauser = new Scanner(System.in);
+
     private boolean closed = false;
 
     public ProgressivePrinter() {
@@ -49,9 +50,22 @@ public class ProgressivePrinter {
         }
     }
 
+    public void pauseWithDelay(int millis) {
+        delay(millis);
+        pause();
+    }
     public void pause() {
-        print(String.format("%nPress [Enter] to continue."));
+        // Instantaneous
+        printStream.printf("%nPress [Enter] to continue.");
         consolePauser.nextLine();
+    }
+    public synchronized void delay(long millis) {
+        try {
+            wait(millis);
+        }
+        catch (InterruptedException waitWasInterruptedException) {
+            waitWasInterruptedException.printStackTrace(System.err);
+        }
     }
 
     public void close() {
@@ -79,7 +93,7 @@ public class ProgressivePrinter {
         progPrinter.setCharLimitPerLine(100);
         progPrinter.print(String.format("%nThere. Everything is in order again! Enjoy some copy pasta!%n"));
 
-        progPrinter.pause();
+        progPrinter.pauseWithDelay(2000);
 
         progPrinter.print(String.format("%n\"The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.\"%n"));
 
